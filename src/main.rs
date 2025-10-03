3a94cb1dcd6cd79fbcacce05d556d82e71e69747
cazzano <cazzano.kiddo@gmail.com>
2025-10-03 17:42:44 +0000

    !important

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use text_io::scan;
 
 fn main() {
+    
+    // Our Brand Function print!() Which Could Be Related To The fmt.Printf() Function by Go.
+
     print!("\nHello, world!\n\n");
+
+    // And We Can Use Let To Add Variables Haah, Relatable To The var by Go.
+
     let gangster = "shelby";
     let lora=6;
+
+    // We Can Use The loop flow, Relateable To The for by Go.
+
     loop{
         print!("\nEnter Your Choice Haah\n\n1.Gangster  2.Lora  3.Run The Function\n\n--> ");
         let choice: i64;
+        
+        // We Can Use The scan!(), Relatable To The fmt.Scanf() By Go.
+        
         scan!("{}",choice);
+         
+        // The Flow Remains The Same As Go.
+
         if choice==1{
             print!("{}",gangster);
         } else if choice==2{
@@ -18,6 +33,8 @@ fn main() {
     }
 
 }
+
+// We Can Make Functions By fn function(), Relatable To The func Functions() By Go.
 
 fn show_values(a: &str,b: &str){
     print!("{}\n\n{}\n",a,b);
