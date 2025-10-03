a6bcef3a08d9f9d13a85f562b1f27f0b071f5c3c
cazzano <cazzano.kiddo@gmail.com>
2025-10-03 17:33:33 +0000

    !important

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,20 @@ fn main() {
     let gangster = "shelby";
     let lora=6;
     loop{
-        print!("\nEnter Your Choice Haah\n\n1.Gangster  2.Lora\n\n--> ");
+        print!("\nEnter Your Choice Haah\n\n1.Gangster  2.Lora  3.Run The Function\n\n--> ");
         let choice: i64;
         scan!("{}",choice);
         if choice==1{
             print!("{}",gangster);
         } else if choice==2{
             print!("{}",lora);
+        } else if choice ==3{
+            show_values("Hi I Am Shelby","And My Company Is An Private Limited");
         } else { break }
     }
 
+}
+
+fn show_values(a: &str,b: &str){
+    print!("\n{}\n\n{}\n",a,b);
 }
