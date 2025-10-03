f526574e6c9037575073cce8b3d0076602d0289e
cazzano <cazzano.kiddo@gmail.com>
2025-10-03 17:21:55 +0000

    !important

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use text_io::scan;
+
 fn main() {
     print!("\nHello, world!\n\n");
     let gangster = "shelby";
-    print!("\n{}\n\n",gangster);
     let lora=6;
-    print!("\n{}\n\n",lora);
+    loop{
+        print!("\nEnter Your Choice Haah\n\n1.Gangster  2.Lora\n\n--> ");
+        let choice: i64;
+        scan!("{}",choice);
+        if choice==1{
+            print!("{}",gangster);
+        } else if choice==2{
+            print!("{}",lora);
+        } else { break }
+    }
+
 }
