712b3498277b1b9a0decf24fc3f64941f2b1a21d
cazzano <cazzano.kiddo@gmail.com>
2025-10-03 17:57:06 +0000

    !important

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     // And We Can Use Let To Add Variables Haah, Relatable To The var by Go.
 
     let gangster = "shelby";
-    let lora=6;
+    let integers=6;
 
     // We Can Use The loop flow, Relateable To The for by Go.
 
     loop{
-        print!("\nEnter Your Choice Haah\n\n1.Gangster  2.Lora  3.Run The Function\n\n--> ");
+        print!("\nEnter Your Choice Haah\n\n1.Gangster  2.Integers  3.Run The Function\n\n--> ");
         let choice: i64;
         
         // We Can Use The scan!(), Relatable To The fmt.Scanf() By Go.
@@ -26,7 +26,7 @@ fn main() {
         if choice==1{
             print!("{}",gangster);
         } else if choice==2{
-            print!("{}",lora);
+            print!("{}",integers);
         } else if choice ==3{
             show_values("Hi I Am Shelby","And My Company Is An Private Limited");
         } else { break }
